e417c6233f50f10da8a48e3e56e0b4ad5a2a69b6
Crowdbotics <team@crowdbotics.com>
2020-06-04 02:29:54 +0000

    Update models

--- backend/tasxit_17770/wsgi.py
+++ backend/tasxit_17770/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tasxit_17770.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tasxit_17770.settings")
 
 application = get_wsgi_application()
