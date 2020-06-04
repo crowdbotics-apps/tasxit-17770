065cdc10ff26f74a91feed3b16cb3dc44436f3d1
Crowdbotics <team@crowdbotics.com>
2020-06-04 02:28:27 +0000

    Initial commit.

---
+++ backend/tasxit_17770/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for tasxit_17770 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tasxit_17770.settings')
+
+application = get_wsgi_application()
